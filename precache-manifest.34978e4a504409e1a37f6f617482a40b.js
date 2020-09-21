82884d1b0ca71c0fa12bf1c358c8699a57a3a553
SIJUZ <sijuz@yandex.ru>
2020-09-21 12:50:16 +0300

    1
    1

---
+++ precache-manifest.34978e4a504409e1a37f6f617482a40b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "742fb864f06f927e19e3e55c8bf27cb0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a1929acc244df6a8f98",
+    "url": "/static/css/2.8c9b10a9.chunk.css"
+  },
+  {
+    "revision": "85afd5b19c749ca52929",
+    "url": "/static/css/main.702b247b.chunk.css"
+  },
+  {
+    "revision": "3a1929acc244df6a8f98",
+    "url": "/static/js/2.1d0168bd.chunk.js"
+  },
+  {
+    "revision": "590b288afc476ced67e9258cf268eacb",
+    "url": "/static/js/2.1d0168bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85afd5b19c749ca52929",
+    "url": "/static/js/main.8212a727.chunk.js"
+  },
+  {
+    "revision": "623fb580824fbe639ae3",
+    "url": "/static/js/runtime-main.cd548ab4.js"
+  },
+  {
+    "revision": "1eddcd7e65fbaae34961c3525cfbb189",
+    "url": "/static/media/fon.1eddcd7e.svg"
+  },
+  {
+    "revision": "8508205779f8eb209ee22a6fbd0ab470",
+    "url": "/static/media/fon_d.85082057.svg"
+  },
+  {
+    "revision": "1446db78832edd707e3322ff35b74f7c",
+    "url": "/static/media/logo.1446db78.svg"
+  },
+  {
+    "revision": "9d8d3085cfcae14c5326cc100c6de93f",
+    "url": "/static/media/logo_d.9d8d3085.svg"
+  },
+  {
+    "revision": "4b483f2b2377e3c88eec888752786fa9",
+    "url": "/static/media/yt.4b483f2b.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.811a285c35391cf90da5402d8faad3e2.js"
+  "/precache-manifest.34978e4a504409e1a37f6f617482a40b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
